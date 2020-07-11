8551c8a873db0048cb9a2b2bc58cfffa0e44c9db
wangwenxin <wangwenxin03@sina.com>
2020-07-11 16:07:41 +0800

    made some change to mysqlprocess.cs

--- mysqlprocess.cs
+++ mysqlprocess.cs
@@ -1,7 +1,7 @@
 public void test()
 {
       int empcode=3;
-      string sql="select * from t_e_org_employee";
-      dataTable dt = object.excute(sql);
+      string sql="select top 30 * from t_e_org_employee";
+      dataTable dt = object.excutes(sql);
 
 }
