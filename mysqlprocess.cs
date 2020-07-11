476da8e5614ff108721ecee9ccb465118f984767
wangwenxin <wangwenxin03@sina.com>
2020-07-11 16:55:17 +0800

    update mysqlprocess.cs by dis_test branch

--- mysqlprocess.cs
+++ mysqlprocess.cs
@@ -1,7 +1,8 @@
 public void test()
 {
       int empcode=3;
-      string sql="select * from t_e_org_employee";
-      dataTable dt = object.excute(sql);
+      string sql="sasdfdsfadselect * from t_e_org_employee";
+      dataTable dt = object.aexcute(sql);
+      sdfasd
 
 }
